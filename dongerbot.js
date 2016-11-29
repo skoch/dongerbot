c30efe98c269bd29621982cde7f9e3f492ad912b
Stephen Koch <stephen.koch@gmail.com>
2016-11-29 11:59:18 -0500

    Added redirect URL to heroku config // kenny loggins

--- dongerbot.js
+++ dongerbot.js
@@ -10,10 +10,6 @@ app.set('port', (process.env.PORT || 3000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/test', function(req, res) {
-    let url = `https://docs.google.com/spreadsheets/d/${process.env.GOOGLE_SPREADSHEET_ID}/edit?usp=sharing`;
-    console.log('url', url);
-});
 app.get('/', function(req, res) {
     // GET where ssl_check set to 1 from Slack
     if (req.query.ssl_check == 1) {
@@ -31,7 +27,7 @@ app.get('/slack-auth', function(req, res) {
 
     request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.redirect('https://s-media-cache-ak0.pinimg.com/originals/b7/7b/8c/b77b8ceaa61f60b6d96cae2e92a3e83e.gif');
+            res.redirect(process.env.THANK_YOU_REDIRECT);
         }
     });
 });
@@ -44,19 +40,20 @@ app.post('/', function(req, res) {
     if (req.body.token == process.env.SLACK_AUTH_TOKEN) {
         console.log('req.body.text', req.body.text);
 
-        var donger = req.body.text ? req.body.text : 'excuseme';
-
+        let text = req.body.text ? req.body.text : 'excuseme';
+        console.log('text', text);
         sheetrock({
             // url: "https://docs.google.com/spreadsheets/d/1QO5dyK6EgIP81SGZMlHMk8xn88u_budzF2Td3OoOZzY/edit#gid=0",
             url: `https://docs.google.com/spreadsheets/d/${process.env.GOOGLE_SPREADSHEET_ID}/edit?usp=sharing`,
-            query: "Select A where B contains'"+ donger +"'",
+            query: `Select A where B = ${text}`,
             reset: true,
             callback: function (error, options, response) {
                 console.log('>>', response.rows.cellsArray[0]);
 
+                let donger = response.rows.cellsArray[0] ? response.rows.cellsArray[0] : ⛔;
                 var response = {
                     "response_type": "in_channel",
-                    "text": response.rows.cellsArray[0],
+                    "text": donger,
                 }
                 res.json(response);
             }
